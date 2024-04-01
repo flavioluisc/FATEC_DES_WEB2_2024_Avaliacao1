8ea96d5a964ca9ef287a760c33856c195d588923
Flávio Luis Coelho <56163731+flavioluisc@users.noreply.github.com>
2024-04-01 17:34:10 -0300

    Update cadastro-carros.php
    Centralizando conteúdo da página

--- cadastro-carros.php
+++ cadastro-carros.php
@@ -15,31 +15,34 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center;}
-        .wrapper{ width: 350px; padding: 20px; }
+        .wrapper{ width: 350px; padding: 20px;}
+        #center{display: flex; flex-direction: row; justify-content: center; align-items: center;}
     </style>
 </head>
 <body>
     <h1>Cadastro de carros</h1>
-    <form action="cadastro-carros2.php" method="post">
-        <div class="form-group">
-            <label>Nome do aluno</label>
-            <input type="text" name="nome" class="form-control">
-            <span class="help-block"></span>
-        </div>    
-        <div class="form-group">
-            <label>RA</label>
-            <input type="text" name="RA" class="form-control">
-            <span class="help-block"></span>
-        </div> 
-        <div class="form-group">
-            <label>Placa</label>
-            <input type="text" name="placa" class="form-control">
-            <span class="help-block"></span>
-        </div> 
-        <div class="form-group">
-            <input type="submit" class="btn btn-success" value="Cadastrar">
-            <a href="welcome.php" class="btn btn-primary">Voltar</a>
-        </div>
-    </form>
+    <div id="center">
+        <form action="cadastro-carros2.php" method="post">
+            <div class="form-group">
+                <label>Nome do aluno</label>
+                <input type="text" name="nome" class="form-control">
+                <span class="help-block"></span>
+            </div>    
+            <div class="form-group">
+                <label>RA</label>
+                <input type="text" name="RA" class="form-control">
+                <span class="help-block"></span>
+            </div> 
+            <div class="form-group">
+                <label>Placa</label>
+                <input type="text" name="placa" class="form-control">
+                <span class="help-block"></span>
+            </div> 
+            <div class="form-group">
+                <input type="submit" class="btn btn-success" value="Cadastrar">
+                <a href="welcome.php" class="btn btn-primary">Voltar</a>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
